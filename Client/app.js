515dceab3b5eab103f9e55012843c739609f2bd2
mitchelln11 <mitchellnaleid@gmail.com>
2019-12-07 14:50:52 -0600

    css added

--- Client/app.js
+++ Client/app.js
@@ -89,7 +89,7 @@
             type: 'get',
             contentType: 'application/json',          
             success: function( data ){
-                $("#response pre").html("<p>" + data.Title + " " + data.Director + " " + data.Genre+"</p>");
+                $("#response pre").html("<table class='gen-info'><tbody><th>ID</th><th>Title</th><th>Director</th><th>Genre</th><tr><td>" + data.MovieId + "</td><td>" + data.Title + "</td><td>" + data.Director + "</td><td>" + data.Genre+"</td></tr>");
 
             },
             error: function( jqXhr, textStatus, errorThrown ){
@@ -105,11 +105,11 @@
     $('#my-getwithparamaterform').submit( getbyidform); 
 
     function WriteToTable(data){
-        var result = "<table class=\"gen-info\"><th>ID</th><th>Title</th><th>Director</th><th>Genre</th>";
+        var result = "<table class='gen-info'><tbody><th>ID</th><th>Title</th><th>Director</th><th>Genre</th>";
             $.each( data, function( index, record ) {
                 result += "<tr><td>" + record.MovieId + "</td><td>" + record.Title + "</td><td>" + record.Director + "</td><td>" + record.Genre + "</td></tr>"
             });
-            result += "</table>"
+            result += "</tbody></table>"
             $("#response pre").html(result);
         }        
 
